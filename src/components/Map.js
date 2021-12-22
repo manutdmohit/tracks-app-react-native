4d12daced336e3a14f5abdc03345a2b5ce67693b
manutdmohit <saudmohit@gmail.com>
2021-12-22 21:01:14 +0545

    showing a series of points

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Polyline } from 'react-native-maps';
 
 const Map = () => {
+  let points = [];
+
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: 28.53848 + i * 0.01,
+        longitude: 81.11612 + i * 0.01,
+      });
+    } else {
+      points.push({
+        latitude: 28.53848 - i * 0.01,
+        longitude: 81.11612,
+      });
+    }
+  }
+
   return (
     <MapView
       style={styles.map}
@@ -12,7 +28,9 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-    />
+    >
+      <Polyline coordinates={points} lineDashPattern={[0]} />
+    </MapView>
   );
 };
 
