200ebcae9a84fd417baa4bc12ce99d48e0504008
manutdmohit <saudmohit@gmail.com>
2021-12-22 14:07:19 +0545

    showing a map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 const Map = () => {
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 28.53848,
+        longitude: 81.11612,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    />
   );
 };
 
 export default Map;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  },
+});
