39a6ea454b33541ce354164b2dea1607430af5f8
manutdmohit <saudmohit@gmail.com>
2021-12-25 20:54:01 +0545

    Drawing a position indicator

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://269a-2400-1a00-b1c0-76f8-39f3-baa2-b5de-fbdd.ngrok.io',
+  baseURL: 'http://de2d-2400-1a00-b1c0-76f8-f863-fd25-919e-8a1a.ngrok.io',
 });
