1ba3ffc354b5213ae9c246680ac30daaf512d1dd
manutdmohit <saudmohit@gmail.com>
2021-12-26 15:15:22 +0545

    automatic disables

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -4,7 +4,6 @@ import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { withNavigationFocus } from 'react-navigation';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
@@ -12,9 +11,7 @@ import useLocation from '../hooks/useLocation';
 const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
 
-  const [err] = useLocation(addLocation);
-
-  console.log(isFocused);
+  const [err] = useLocation(isFocused, addLocation);
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
