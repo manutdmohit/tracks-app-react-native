2000e91c49a0cacf954fe159f7e0cb08892b404f
manutdmohit <saudmohit@gmail.com>
2021-12-26 14:51:02 +0545

    trying to disable location tracking

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,16 +2,19 @@
 import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
+import { withNavigationFocus } from 'react-navigation';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
 
   const [err] = useLocation(addLocation);
+
+  console.log(isFocused);
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
@@ -24,4 +27,4 @@ const TrackCreateScreen = () => {
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
