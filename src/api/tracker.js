c7d08ef432432c3724d413605beb79dc55742753
manutdmohit <saudmohit@gmail.com>
2021-12-26 13:38:54 +0545

    extracting logic to a hook

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://de2d-2400-1a00-b1c0-76f8-f863-fd25-919e-8a1a.ngrok.io',
+  baseURL: 'http://1f03-2400-1a00-b1c0-76f8-81d7-752c-9385-4852.ngrok.io',
 });
