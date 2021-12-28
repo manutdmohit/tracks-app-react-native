3b5c0e55474b6d0f4be1f75f88574bc41a40a77e
manutdmohit <saudmohit@gmail.com>
2021-12-28 13:13:03 +0545

    Save Button

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -11,8 +11,6 @@ const TrackForm = () => {
     changeName,
   } = useContext(LocationContext);
 
-  console.log(locations.length);
-
   return (
     <>
       <Spacer>
@@ -27,6 +25,12 @@ const TrackForm = () => {
         ) : (
           <Button title="Start Recording" onPress={startRecording} />
         )}
+      </Spacer>
+
+      <Spacer>
+        {!recording && locations.length ? (
+          <Button title="Save Recording" />
+        ) : null}
       </Spacer>
     </>
   );
