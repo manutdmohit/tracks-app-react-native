8bbcd06c282ec73de274e1fb69a5c943bf7c7698
manutdmohit <saudmohit@gmail.com>
2021-12-29 20:05:04 +0545

    added backend webisite

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const instance = axios.create({
-  baseURL: 'http://6f85-2400-1a00-b1c0-262b-9015-7f18-cb6c-7982.ngrok.io',
+  baseURL: 'https://trackerappbymohit.herokuapp.com',
 });
 
 instance.interceptors.request.use(
